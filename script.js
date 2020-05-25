e41914f45d02140ce92b6989de17997fd06c2001
Artur Rymsza <33729068+arturrymsza@users.noreply.github.com>
2020-05-25 18:29:09 +0200

    Add options

--- script.js
+++ script.js
@@ -2,4 +2,30 @@ console.log('ml5 version:', ml5.version);
 
 let model;
 let targetLabel = 'C';
+var canvas = document.createElement("CANVAS");
 
+let state = 'collection';
+
+function setup() {
+    createCanvas(600,600);
+    let options = {
+    inputs: ['x', 'y'],
+    outputs: ['label'],
+    task: 'classification',
+    debug: true
+    };
+    background(245);
+}
+
+
+
+model = ml5.neuralNetwork(options);
+
+function mousePressed() {
+    let inputs = {
+        x: mouseX,
+        y: mouseY
+    };
+
+
+}
